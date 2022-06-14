267451c329e26be297dedecca8918ebdd41d79af
Sudh9003 <sudhanshusinha@gmail.com>
2022-06-14 09:32:05 +0530

    New Function

--- src/Password.java
+++ src/Password.java
@@ -13,11 +13,17 @@ public class Password {
 		System.out.println("Doing Good");
 		System.out.println("Havyfk");
 		System.out.println("srgbbary");
+		System.out.println("srgbbary");
+		
 		int i;
 		for(i=0;i<name.length;i++);
 		{
 		System.out.print(name[i]);
 		//System.out.print(Arrays.toString(name));
 		}
+		
+	}
+	public void newFunction() {
+		System.out.println("New Function Added");
 	}
 	}
